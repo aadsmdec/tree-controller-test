c3679f4ca80ae390e00502656981c762a9a4c8fa
Afonso <noreply>
2014-04-03 23:38:43 +0000

    Update component /ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,35 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+    
+    createDataTree: {
+        value: function(treeShape) {
+            var treeRoot = {};
+            var children = [];
+            
+        }
+    },
+    
+    createDataTreeNode: {
+        value: function(shape) {
+            var childrenCount = shape[0];
+            var childrenShape;
+            var children;
+
+            if (childrenCount) {
+                childrenShape = shape.slice(1);
+                children = new Array(childrenCount);
+                for (var i = 0; i < childrenCount; i++) {
+                    children[i] = this.createDataTreeNode(childrenShape);
+                }
+            }
+        }
+    },  
+    
+    handleCreateTreeButtonAction: {
+        value: function(event) {
+            this.createDataTree(event.detail.get("treeShape"));
+        }
     }
 });
