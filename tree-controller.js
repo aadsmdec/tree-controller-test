65bf13c8f165f2cf73ddef38753a6fe5e2b22d25
Afonso <noreply>
2014-04-04 17:51:07 +0000

    Update component /tree-controller.js

--- tree-controller.js
+++ tree-controller.js
@@ -10,7 +10,7 @@ exports.TreeNodeController = Montage.specialize({
             var iterations = [],
                 childrenPath,
                 childrenContent;
-            
+
             this._controller = controller;
             this.parent = parent;
             this.depth = depth;
@@ -19,30 +19,30 @@ exports.TreeNodeController = Montage.specialize({
             } else {
                 this._expanded = controller.initiallyExpanded || false;
             }
-            
+
             childrenPath = "content." + (controller.childrenPath||"children");
             this.addRangeAtPathChangeListener(childrenPath, this, "handleChildrenContentChange");
             this._content = content;
             childrenContent = this.getPath(childrenPath);
-            
+
             this.children = childrenContent.map(function(content) {
                 var child = new this.constructor(controller, this, content, depth + 1);
-                
+
                 iterations.push(child);
                 if (child.expanded) {
                     iterations = iterations.concat(child.iterations);
                 }
                 return child;
             }, this);
-            
+
             this.iterations = iterations;
         }
     },
-    
+
     _content: {
-        value: null
+        value: undefined
     },
-    
+
     content: {
         get: function() {
             return this._content;
@@ -51,11 +51,11 @@ exports.TreeNodeController = Montage.specialize({
             this._content = value;
         }
     },
-    
+
     _expanded: {
         value: false
     },
-    
+
     expanded: {
         get: function() {
             return this._expanded;
@@ -73,17 +73,17 @@ exports.TreeNodeController = Montage.specialize({
             }
         }
     },
-    
+
     _findIterationsIndex: {
         value: function() {
             var node = this;
             var iterationsIndex = 0;
-            
+
             while (node.parent && node.parent.expanded) {
                 iterationsIndex += node.parent.iterations.indexOf(node) + 1;
                 node = node.parent;
             }
-            
+
             if (node.parent) {
                 return -1;
             } else {
@@ -91,7 +91,7 @@ exports.TreeNodeController = Montage.specialize({
             }
         }
     },
-    
+
     _collapse: {
         value: function() {
             var node = this;
@@ -103,14 +103,14 @@ exports.TreeNodeController = Montage.specialize({
                 node.parent.iterations.splice(iterationsIndex + 1, iterationsCount);
                 node = node.parent;
             }
-            
+
             if (!node.parent) {
                 iterationsIndex = this._controller.iterations.indexOf(this);
                 this._controller.iterations.splice(iterationsIndex + 1, iterationsCount);
             }
         }
     },
-    
+
     _expand: {
         value: function() {
             var node = this;
@@ -131,7 +131,7 @@ exports.TreeNodeController = Montage.specialize({
             }
         }
     },
-    
+
     handleChildrenContentChange: {
         value: function(plus, minus, index) {
             debugger
@@ -148,9 +148,9 @@ exports.TreeController = Montage.specialize(/** @lends TreeController# */ {
     constructor: {
         value: function TreeController(content, childrenPath, initiallyExpanded, expandedPath) {
             var iterations;
-            
+
             this.super();
-            
+
             this.childrenPath = childrenPath;
             this.expandedPath = expandedPath;
             this.initiallyExpanded = initiallyExpanded;
@@ -160,19 +160,19 @@ exports.TreeController = Montage.specialize(/** @lends TreeController# */ {
             this.iterations = iterations;
         }
     },
-    
+
     childrenPath: {
         value: null
     },
-    
+
     expandedPath: {
         value: null
     },
-    
+
     iterations: {
         value: null
     },
-    
+
     NodeController: {
         value: exports.TreeNodeController
     }
