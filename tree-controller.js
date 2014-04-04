f6bdeb75e1f43f97cdee7b736daa35e72aaff4c1
Afonso <noreply>
2014-04-04 03:01:20 +0000

    Add module tree-controller

---
+++ tree-controller.js
@@ -0,0 +1,16 @@
+/**
+ * @module tree-controller
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class TreeController
+ * @extends Montage
+ */
+exports.TreeController = Montage.specialize(/** @lends TreeController# */ {
+    constructor: {
+        value: function TreeController() {
+            this.super();
+        }
+    }
+});
